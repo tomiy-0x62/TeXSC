402188ba9c7703a04f9290979e4fe495f392ea1b
Ryosuke <ryosuke1538>
2022-05-08 07:58:32 +0900

    add regex & add struct Lexer

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,52 @@ use clap::{Command, Arg};
 use std::fs::File;
 use std::io::{BufReader, BufRead, stdout, Write};
 use std::io;
+use regex::Regex;
 
-fn tex_parser(form: String) {
-    println!("form: {}", form.replace("\n", " "));
+struct Lexer {
+    formulas: String,
+    tokens: Vec<String>
+}
+
+/*
+tokenの種類
+- 数値リテラル: 0x54, 0b100011, 534
+- TeXコマンド: \log, \sum
+- コマンド: sin, cos
+- かっこ: (, ), {, }
+- 変数: x, y
+*/
+
+impl Lexer {
+    fn new(form: String) -> Lexer {
+        Lexer { formulas: form, tokens: Vec::new() }
+    }
+
+    fn print_form(&self) {
+        println!("form: {}", self.formulas.replace("\n", " "));
+    }
+
+    fn analyze(&mut self) {
+        let tex_command = Regex::new(r"\\[A-Za-z]*").unwrap(); // OK
+        let operator = Regex::new(r"\+|-|/|\|").unwrap(); // OK
+        let command = Regex::new(r"sin|cos|tan|arcsin|arccons|arctan").unwrap(); // OK
+        let var = Regex::new(r"[A-Za-z][A-Za-z0-9]*").unwrap(); // OK
+        let num = Regex::new(r"0x[0-9]+|0b[0-1]+|[0-9]+\.?[0-9]*").unwrap(); //
+        let braces = Regex::new(r"\(|\)|\[|\]|\{|\}").unwrap(); // OK
+        let token_types: Vec<Regex> = [tex_command, operator, command, var, num, braces].to_vec();
+
+        
+        /*
+        for caps in tex_command.captures_iter(&self.formulas) {
+            println!("match '{}'", &caps[0]);
+        }*/
+        
+        /* if let Some(caps) = braces.captures(&self.formulas) {
+            println!("match '{}'", caps.get(0).unwrap().as_str());
+            // if let Some(hoge) = caps.get(0)
+        }*/
+
+    }
 }
 
 fn main_loop() {
@@ -19,7 +62,9 @@ fn main_loop() {
         if form.replace("\n", "").as_str() == "exit" {
             return;
         }
-        tex_parser(form);
+        let mut lex = Lexer::new(form.to_string());
+        lex.print_form();
+        lex.analyze();
     }
 }
 
@@ -42,7 +87,9 @@ fn main() {
     
     // formulas from command line arg
     if let Some(form) = matches.value_of("tex formulas") {
-        tex_parser(form.to_string());
+        let mut lex = Lexer::new(form.to_string());
+        lex.print_form();
+        lex.analyze();
         return;
     }
 
@@ -51,7 +98,9 @@ fn main() {
         let f = File::open(file_name).expect(file_name);
         let reader = BufReader::new(f);
         for result in reader.lines() {
-            tex_parser(result.unwrap());
+            let mut lex = Lexer::new(result.unwrap());
+            lex.print_form();
+            lex.analyze();
         }
         return;
     }
