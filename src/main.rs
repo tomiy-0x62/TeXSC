0e66566cf79a3ec7e676c505b959dbea620a79af
Ryosuke <ryosuke1538>
2022-05-07 20:01:44 +0900

    add file to arg

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,23 @@ fn main() {
         .author("tomiy <tomiy@tomiylab.com>")
         .about("TeXSC: TeX Scientific Calculator") 
         .arg(Arg::new("formulas")
-        .help("Szpecify the formula to  calculate")
+        .help("Szpecify the formula to calculate")
         .short('c')
         .takes_value(true)
-    );
+        )
+        .arg(Arg::new("file")
+        .help("load formulas from file")
+        .required(false)
+        );
 
     let matches = app.get_matches();
 
     if let Some(form) = matches.value_of("formulas") {
         println!("formulas: {}", form);
+    }
+
+    if let Some(file) = matches.value_of("file") {
+        println!("file: {}", file);
     }
 
     println!("Hello, world!");
